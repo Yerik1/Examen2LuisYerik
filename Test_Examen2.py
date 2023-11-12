23cc89ecd23f4286a2693362c598ee55782f8b71
Luis Vargas <luisalejandrovb@gmail.com>
2023-11-12 16:29:01 -0600

    Agregar metodos a las pruebas unitarias, la obtencion de la mas bailable y verifiacacion de la lista de canciones

--- Test_Examen2.py
+++ Test_Examen2.py
@@ -20,6 +20,22 @@ class MyTestCase(unittest.TestCase):
         objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertEqual(objeto.DivisibleTempo(20), [1, 2, 4, 5, 10, 20])  # add assertion here
 
+    ###LUIS###
+    def test_ObtieneMasBailable1(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)  # add assertion here
+
+    def test_ObtieneMasBailable2(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.ObtieneMasBailable([1, 2, 4, 5, 10, 20]), 20)  # add assertion here
+
+    def test_VerificaListaCanciones1(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]), True)
+
+    def test_VerificaListaCanciones2(self):
+        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(objeto.VerificaListaCanciones([1, 2, 4, 5, 10, 20]), True)
 
 if __name__ == '__main__':
     unittest.main()
